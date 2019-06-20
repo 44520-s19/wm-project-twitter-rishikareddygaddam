73f15cf6457101d772dfe1700890e5c2e793902d
Rishika Reddy Gaddam <rishika.reddy.g5@gmail.com>
2019-06-19 21:35:58 -0500

    completed

---
+++ nltk_helpers.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 13:26:57 2019
+
+@author: S534665
+"""
+
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+def get_sentiments(text):  
+   analyzer = SentimentIntensityAnalyzer()
+   return analyzer.polarity_scores(text)
+
+
+def split_sentiments(sentiments):
+    xs=[sent['neg'] for sent in sentiments]
+    ys=[sent['neu'] for sent in sentiments]
+    zs=[sent['pos'] for sent in sentiments]    
+    return xs,ys,zs
